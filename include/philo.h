9bf8c8ff2585ba332d8e426fd4fb43c1bbc6d9cd
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-07-25 11:52:42 +0200

    feat: basic header

--- include/philo.h
+++ include/philo.h
@@ -1,20 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/07/24 11:24:17 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/07/25 11:52:15 by rgohrig          ###   ########.fr       */
+/*   Created: 2025/05/15 11:17:46 by rgohrig           #+#    #+#             */
+/*   Updated: 2025/07/25 11:52:06 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "philo.h"
+#ifndef PHILO_H
+# define PHILO_H
 
-int	main(void)
-{
+# include <pthread.h>  // pthreads and mutex
+# include <stdbool.h>  // bool, true, false
+# include <stdint.h>   // uint64_t, int64_t, etc.
+# include <stdio.h>    // printf
+# include <stdlib.h>   // malloc, free
+# include <string.h>   // memset
+# include <sys/time.h> // gettimeofday
+# include <unistd.h>   // write, usleep
 
-	return (0);
+// auto
 
-}
+#endif
