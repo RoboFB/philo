a4accd730bc5b45a0c52e122bd4fabe642d23b62
“rgohrig” <rgohrig@student.42heilbronn.de>
2025-08-29 16:25:13 +0200

    feat: new main logic

--- src/single.c
+++ src/single.c
@@ -6,114 +6,83 @@
 /*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 16:40:30 by rgohrig           #+#    #+#             */
-/*   Updated: 2025/08/26 19:23:51 by rgohrig          ###   ########.fr       */
+/*   Updated: 2025/08/29 15:56:55 by rgohrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	change_print_state(t_phil *phil, t_print_state new_state)
-{
-	int	return_value;
-	
-	return_value = 0;
-	pthread_mutex_lock(&phil->data->stop_sim_mtx);
-	if (phil->data->stop_simulation)
-		return_value = ERROR;
-	pthread_mutex_unlock(&phil->data->stop_sim_mtx);
-
-	if (return_value == 0)
-	{
-		print_state(phil);
-	}
-	return (return_value);
-}
-
-
-int	take_print_fork(t_phil *phil, pthread_mutex_t *fork)
-{
-	int	return_value;
-	
-	return_value = 0;
-	pthread_mutex_lock(&phil->data->stop_sim_mtx);
-	if (phil->data->stop_simulation)
-		return_value = ERROR;
-	pthread_mutex_unlock(&phil->data->stop_sim_mtx);
-
-	if (return_value == 0)
-	{
-		pthread_mutex_lock(fork);
-		print_fork(phil);
-	}
-	return (return_value);
-}
-
-
+// R: (0)Normal  (-1)stop sim
 static int	h_think(t_phil *phil)
 {
-	if (change_print_state(phil, PR_THINKING) == ERROR)
-		return (ERROR);
-	if (phil->id % 2 == 0)
+	return (check_print(phil, PR_THINKING));
+}
+
+// R: (0)Normal  (-1)stop sim
+static int	h_forks(t_phil *phil)
+{
+	if (*phil->id % 2 == 0 && phil->data->total_philos % 2 == 0)
 	{
-		if (phil->eat_count == 0
-			&& sleep_exact_ms(phil->data, (phil->data->eat_ms) / 2) == ERROR)
-			return (ERROR);
-		if (take_print_fork(phil, phil->fork_left_mtx) == ERROR
-			|| take_print_fork(phil, phil->fork_right_mtx) == ERROR)
+		if (take_print_fork(phil, phil->fork_left_mtx, phil->fork_left) == ERROR
+			|| take_print_fork(phil, phil->fork_right_mtx, phil->fork_right) == ERROR)
 			return (ERROR);
 	}
 	else
 	{
-		if (take_print_fork(phil, phil->fork_right_mtx) == ERROR
-		|| take_print_fork(phil, phil->fork_left_mtx) == ERROR)
-		return (ERROR);
+		if (take_print_fork(phil, phil->fork_right_mtx, phil->fork_right) == ERROR
+		|| take_print_fork(phil, phil->fork_left_mtx, phil->fork_left) == ERROR)
+			return (ERROR);
 	}
 	return (0);
 }
+
+// R: (0)Normal  (-1)stop sim
 static int	h_eat(t_phil *phil)
 {
-	int	return_value;
-
-	if (change_print_state(phil, PR_EATING) == ERROR)
-		return (ERROR);
-	return_value = 0;
-	pthread_mutex_lock(&phil->data->eat_mtxs);
+	pthread_mutex_lock(phil->eat_mtx);
+	if (check_stop_sim(phil->data) == ERROR)
+		return (pthread_mutex_unlock(phil->eat_mtx), ERROR);
 	(void)gettimeofday(phil->eat_timestamp, NULL);
-	pthread_mutex_unlock(&phil->data->eat_mtxs);
+	pthread_mutex_unlock(phil->eat_mtx);
+	
+	
+	if (check_print(phil, PR_EATING) == ERROR)
+		return (ERROR);
+
 	if (sleep_exact_ms(phil->data, phil->data->eat_ms) == ERROR)
-		return_value = ERROR;
-	pthread_mutex_unlock(phil->fork_right_mtx);
-	pthread_mutex_unlock(phil->fork_left_mtx);
-	return (return_value);
+		return (ERROR);
+	
+	release_forks(phil);
+	
+	pthread_mutex_lock(phil->eat_mtx);
+	if (check_stop_sim(phil->data) == ERROR)
+		return (pthread_mutex_unlock(phil->eat_mtx), ERROR);
+	(*phil->eat_count)++;
+	pthread_mutex_unlock(phil->eat_mtx);
+	return (0);
 }
+
+// R: (0)Normal  (-1)stop sim
 static int	h_sleep(t_phil *phil)
 {
-	if (change_print_state(phil, PR_SLEEPING) == ERROR)
+	if (check_print(phil, PR_SLEEPING) == ERROR)
 		return (ERROR);
 	if (sleep_exact_ms(phil->data, phil->data->sleep_ms) == ERROR)
 		return (ERROR);
 	return (0);
 }
 
-// todo error handeling returns
+// R: NULL
 void	*single(void *phil_arg)
 {
 	t_phil	*phil;
 
 	phil = (t_phil *)phil_arg;
-	while (phil->eat_count < phil->data->max_eat_count)
-	{
+	while (1)
 		if (h_think(phil) == ERROR
+			|| h_forks(phil) == ERROR
 			|| h_eat(phil) == ERROR
 			|| h_sleep(phil) == ERROR)
-			return (NULL);
-		phil->eat_count++;
-	}
-	if (phil->eat_count == phil->data->max_eat_count) // needs better handeling of braking if has finsed eating mab make it to enum with SOTP_EAT STOP_DEAD
-	{
-		pthread_mutex_lock(&phil->data->stop_sim_mtx);
-		phil->data->stop_simulation = true;
-		pthread_mutex_unlock(&phil->data->stop_sim_mtx);
-	}
+			break ;
 	return (NULL);
 }
---
+++ src/use_forks.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   use_forks.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgohrig <rgohrig@student.42heilbronn.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 17:11:02 by rgohrig           #+#    #+#             */
+/*   Updated: 2025/08/29 15:46:58 by rgohrig          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "philo.h"
+
+// R: (0)Normal  (-1)stop sim
+int	take_print_fork(t_phil *phil, pthread_mutex_t *fork_mtx, bool *fork)
+{
+	while (1)
+	{
+		pthread_mutex_lock(fork_mtx);
+		if (check_stop_sim(phil->data) == ERROR)
+			return (pthread_mutex_unlock(fork_mtx), ERROR);
+		if (*fork == false)
+		{
+			*fork = true;
+			pthread_mutex_unlock(fork_mtx);
+			break ;
+		}
+		else
+		{
+			pthread_mutex_unlock(fork_mtx);
+		}
+		usleep(30);
+	}
+	return (check_print(phil, PR_FORK));
+}
+
+// R: void
+void	release_forks(t_phil *phil)
+{
+	pthread_mutex_lock(phil->fork_right_mtx);
+	*phil->fork_right = false;
+	pthread_mutex_unlock(phil->fork_right_mtx);
+	pthread_mutex_lock(phil->fork_left_mtx);
+	*phil->fork_left = false;
+	pthread_mutex_unlock(phil->fork_left_mtx);
+	return ;
+}
